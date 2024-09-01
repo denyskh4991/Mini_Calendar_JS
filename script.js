24dca43f3d3c848235c2f36254b6a9322389d16a
denyskh4991 <denyskh4991@gmail.com>
2024-09-01 22:08:16 +0200

    Adding DOM elements to show the current day

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ const year = document.getElementById("year");
 
 const today = new Date();
 
-console.log(today);
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+date.innerHTML = today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
