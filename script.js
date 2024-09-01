c3d9f12b2892726edd85c91d36ed67117ff80d60
denyskh4991 <denyskh4991@gmail.com>
2024-09-01 21:59:46 +0200

    Adding displaying in browser console current date

---
+++ script.js
@@ -0,0 +1,8 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+console.log(today);
